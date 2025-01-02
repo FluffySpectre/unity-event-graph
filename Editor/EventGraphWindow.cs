b0ab688c70022e3ea303b50ea07ef7c7736cb8b5
Sennio <b.bosse1991@gmail.com>
2025-01-02 11:04:30 +0100

    Revert "Improved port highlighting"
    This reverts commit 26ba928a059b1b53a1a9d17d3761d71f1b153eb2.
    
    # Conflicts:
    #	Editor/EventGraphWindow.cs

--- Editor/EventGraphWindow.cs
+++ Editor/EventGraphWindow.cs
@@ -405,7 +405,6 @@ namespace FluffySpectre.UnityEventGraph
             foreach (var kvp in _highlightedPorts)
             {
                 kvp.Key.portColor = Color.white;
-                kvp.Key.contentContainer.Q<Label>().style.color = Color.white;
             }
             _highlightedPorts.Clear();
         }
@@ -432,8 +431,6 @@ namespace FluffySpectre.UnityEventGraph
             foreach (var edge in edges)
             {
                 HighlightEdge(edge, duration);
-
-                HighlightPort(edge.input as UnityEventPort, duration);
 
                 var targetNode = edge.input.node as UnityEventNode;
                 if (targetNode != null)
@@ -496,7 +493,6 @@ namespace FluffySpectre.UnityEventGraph
             foreach (var port in portsToReset)
             {
                 port.portColor = Color.white;
-                port.contentContainer.Q<Label>().style.color = Color.white;
                 _highlightedPorts.Remove(port);
             }
         }
@@ -527,8 +523,6 @@ namespace FluffySpectre.UnityEventGraph
         private void HighlightPort(UnityEventPort port, double duration = 1.0)
         {
             port.portColor = Color.yellow;
-            port.contentContainer.Q<Label>().style.color = Color.yellow;
-
             double resetTime = EditorApplication.timeSinceStartup + duration;
 
             if (!_highlightedPorts.ContainsKey(port) || _highlightedPorts[port] < resetTime)
